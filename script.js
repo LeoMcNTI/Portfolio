2db94b33b57606591136aceab061d36ba3418f4e
Stringmare <leomcclure07@gmail.com>
2026-09-02 09:55:01 +0200

    Added a pigeon pet counter. Changed images to WebP.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const button = document.querySelector("#themeButton");
+const pigeon = document.querySelector(".pigeon");
+const petCounter = document.querySelector("#pet-counter");
+let pets = 0;
 
 button.addEventListener("click", function() {
     document.body.classList.toggle("dark");
+});
+
+pigeon.addEventListener("click", () => {
+    pets++;
+    petCounter.textContent = `Pets: ${pets}`;
+    pigeon.title = `You have petted the pigeon ${pets} time${pets === 1 ? "" : "s"}.`;
+
+    petCounter.classList.remove("pop");
+    void petCounter.offsetWidth;
+    petCounter.classList.add("pop");
 });
